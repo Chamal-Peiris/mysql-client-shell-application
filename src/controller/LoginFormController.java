14a68c8f54c80aa652329947af071749996c2f4d
Chamal Peiris <chamal.peiris3g@gmail.com>
2022-02-25 12:48:53 +0530

    Build Login Form

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+    public TextField txtHost;
+    public TextField txtPort;
+    public TextField txtUsername;
+    public TextField txtPassword;
+    public Button btnConnect;
+    public void initialize() {
+        Platform.runLater(()->txtUsername.requestFocus());
+    }
+
+    public void btnConnect_OnAction(ActionEvent actionEvent) {
+
+    }
 }
