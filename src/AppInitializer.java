caa46eee75b7ac3e70165a71f933001a3c474007
Chamal Peiris <chamal.peiris3g@gmail.com>
2022-02-25 12:57:32 +0530

    Validate User Inputs On Login Form

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
 
@@ -8,7 +13,14 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        AnchorPane root = FXMLLoader.load(this.getClass().getResource("/view/LoginForm.fxml"));
+        Scene loginScene = new Scene(root);
+        primaryStage.setScene(loginScene);
+        primaryStage.setTitle("MySQL Client Shell Application: Login");
+        primaryStage.setResizable(false);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+        primaryStage.centerOnScreen();
     }
 }
--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -2,8 +2,11 @@ package controller;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public TextField txtHost;
@@ -17,5 +20,39 @@ public class LoginFormController {
 
     public void btnConnect_OnAction(ActionEvent actionEvent) {
 
+        /* Let's validate some inputs */
+        if (txtHost.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Host can't be empty").show();
+            txtHost.requestFocus();
+            txtHost.selectAll();
+            return;
+        }else if (!txtPort.getText().matches("\\d+")){
+            new Alert(Alert.AlertType.ERROR, "Invalid port").show();
+            txtPort.requestFocus();
+            txtPort.selectAll();
+            return;
+        }else if (txtUsername.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Username can't be empty").show();
+            txtUsername.requestFocus();
+            txtUsername.selectAll();
+            return;
+        }
+
+        String command = String.format("mysql -h %s -u %s -p%s --port %s",
+                txtHost.getText(),
+                txtUsername.getText(),
+                txtPassword.getText(),
+                txtPort.getText());
+        try {
+            Process mysql = Runtime.getRuntime().exec(command);
+            System.out.println(mysql.waitFor());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void btnExit_OnAction(ActionEvent actionEvent) {
+        System.exit(0);
     }
 }
